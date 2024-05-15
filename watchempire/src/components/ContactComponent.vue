1f4792e109317f086a0b35b586b43391213de63b
Your Name <you@example.com>
2024-05-15 19:30:43 -0300

    Pagina contacto terminada

--- watchempire/src/components/ContactComponent.vue
+++ watchempire/src/components/ContactComponent.vue
@@ -1,15 +1,15 @@
 <template>
   <section>
     <h1 class="text-4xl text-center my-12">Contacto</h1>
-    <div class="w-5/6 min-h-0 m-auto mb-12 pb-8 flex">
-      <div class="w-3/5 h-full px-5 border-r-2 border-gray-400">
+    <div class="w-5/6 min-h-0 m-auto mb-12 pb-8 flex flex-col lg:flex-row">
+      <div class="w-full lg:w-3/5 h-full lg:px-5 lg:border-r-2 lg:border-gray-400">
         <h3 class="text-2xl text-center my-4">Enviar mensaje</h3>
-        <p class="">
+        <p class="text-justify">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam a purus
           id ex rhoncus dictum. Integer viverra luctus elementum.
         </p>
         <div
-          class="w-full h-full mt-5 grid grid-cols-2 grid-rows-2 gap-4"
+          class="w-full h-full mt-5 grid grid-cols-1 lg:grid-cols-2 lg:grid-rows-2 gap-4"
         >
           <div>
             <label
@@ -100,40 +100,66 @@
           <label
             for="input-group-1"
             class="block mb-2 text-sm font-medium text-gray-900"
-            >Asunto</label
+            >Mensaje</label
           >
           <textarea class="w-full h-44 p-5 bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block"></textarea>
             <button class="float-right bg-black hover:bg-gray-900 mt-5 text-white text-xl rounded py-3 px-8"><i class="fas fa-arrow-right"></i> Enviar</button>
         </div>
       </div>
+
+      <div class="w-full lg:w-2/5 h-full mt-12 lg:mt-0 px-5 flex flex-col items-center justify-between">
+        <div class="w-full min-h-0 mb-12">
+            <h3 class="text-xl my-3">llamanos</h3>
+            <p class="text-sm text-gray-700 mb-4">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam a purus id ex rhoncus dictum. Integer viverra luctus elementum.
+            </p>
+            <p class="text-orange-800 flex items-center font-bold"><i class="w-9 h-9 leading-9 text-center mr-3 fas fa-phone border border-orange-500 bg-orange-400 rounded-full"></i> (54 11) 9999-9999</p>            
+        </div>
+
+        <div class="w-full min-h-0 mb-12">
+            <h3 class="text-xl my-3">Visitanos</h3>
+            <p class="text-sm text-gray-700 mb-4">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam a purus id ex rhoncus dictum. Integer viverra luctus elementum.
+            </p>
+            <p class="text-orange-800 flex items-center font-bold"><i class="w-9 h-9 leading-9 text-center mr-3 fas fa-location border border-orange-500 bg-orange-400 rounded-full"></i> Av. Lorem Ipsum 3400 - CABA</p>            
+        </div>
+
+        <div class="w-full min-h-0">
+            <h3 class="text-xl my-3">Chat en vivo</h3>
+            <p class="text-sm text-gray-700 mb-4">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam a purus id ex rhoncus dictum. Integer viverra luctus elementum.
+            </p>
+            <p class="text-orange-800 flex items-center font-bold"><i class="w-9 h-9 leading-9 text-center mr-3 fas fa-video border border-orange-500 bg-orange-400 rounded-full"></i> Live chat</p>            
+        </div>
+      </div>  
     </div>
 
     
   
-    <iframe class="w-5/6 m-auto my-12" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d26265.10627243174!2d-58.3958528!3d-34.6259456!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bccad68fbbfe49%3A0x690c1074e08ab743!2sBelgrano!5e0!3m2!1ses-419!2sar!4v1715694894905!5m2!1ses-419!2sar" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    <iframe class="w-5/6 h-44 lg:h-64 m-auto my-12" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d26265.10627243174!2d-58.3958528!3d-34.6259456!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bccad68fbbfe49%3A0x690c1074e08ab743!2sBelgrano!5e0!3m2!1ses-419!2sar!4v1715694894905!5m2!1ses-419!2sar" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
   
-    <div class="w-5/6 min-h-0 m-auto my-12 pb-8 flex justify-between">
-        <h2 class="text-5xl">
+    <div class="w-5/6 min-h-0 m-auto mt-28 mb-12 pb-8 flex flex-col lg:flex-row justify-between">
+        <h2 class="text-3xl text-center lg:text-left lg:text-4xl">
             Preguntas frecuentes
         </h2>
-        <div class="w-3/6 min-h-0">
-            <div @click="toggleAcc1" class="w-full h-16 duration-300 bg-cyan-400 hover:bg-cyan-300 px-8 rounded-t-lg cursor-pointer flex items-center justify-between">
-                <h3 class="text-xl">¿Cuál es su política de devolución?</h3>
-                <i class="fas fa-arrow-circle-down text-2xl"></i>
+        <div class="w-full lg:w-3/6 min-h-0 mt-12 lg:mt-0">
+            <div @click="toggleAcc1" class="w-full h-16 duration-300 hover:bg-gray-100 border-b border-gray-800 px-8 cursor-pointer flex items-center justify-between">
+                <h3 class="text-sm md:text-xl">¿Cuál es su política de devolución?</h3>
+                <i :class="{ 'fa-minus': acc1, 'fa-plus': !acc1 }" class="fas text-base lg:text-2xl"></i>
             </div>
-            <div id="acc1" :class="{ 'acc1Show': acc1, 'acc1Hidden': !acc1 }" class="bg-stone-200">
+            <div id="acc1" :class="{ 'acc1Show': acc1, 'acc1Hidden': !acc1 }" class="bg-stone-200 mb-2">
                 <p class="p-3">Aceptamos devoluciones dentro de los 30 días posteriores a la compra, siempre que el reloj esté en su estado original.</p>
             </div>
-            <div @click="toggleAcc2" class="w-full h-16 duration-300 bg-cyan-400 hover:bg-cyan-300 px-8 rounded-t-lg cursor-pointer flex items-center justify-between">
-                <h3 class="text-xl">¿Cómo puedo seguir mi pedido?</h3>
-                <i class="fas fa-arrow-circle-down text-2xl"></i>
+            <div @click="toggleAcc2" class="w-full h-16 duration-300 hover:bg-gray-100 border-b border-gray-800 px-8 cursor-pointer flex items-center justify-between">
+                <h3 class="text-sm md:text-xl">¿Cómo puedo seguir mi pedido?</h3>
+                <i :class="{ 'fa-minus': acc2, 'fa-plus': !acc2 }" class="fas text-base lg:text-2xl"></i>
             </div>
-            <div id="acc2" :class="{ 'acc2Show': acc2, 'acc2Hidden': !acc2 }" class="bg-stone-200">
+            <div id="acc2" :class="{ 'acc2Show': acc2, 'acc2Hidden': !acc2 }" class="bg-stone-200 mb-2">
                 <p class="p-3">Una vez que tu pedido sea enviado, recibirás un correo electrónico con un enlace de seguimiento para rastrear tu paquete en tiempo real.</p>
             </div>
-            <div @click="toggleAcc3" class="w-full h-16 duration-300 bg-cyan-400 hover:bg-cyan-300 px-8 rounded-t-lg cursor-pointer flex items-center justify-between">
-                <h3 class="text-xl">¿Ofrecen garantía en sus relojes?</h3>
-                <i class="fas fa-arrow-circle-down text-2xl"></i>
+            <div @click="toggleAcc3" class="w-full h-16 duration-300 hover:bg-gray-100 border-b border-gray-800 px-8 cursor-pointer flex items-center justify-between">
+                <h3 class="text-sm md:text-xl">¿Ofrecen garantía en sus relojes?</h3>
+                <i :class="{ 'fa-minus': acc3, 'fa-plus': !acc3 }" class="fas text-base lg:text-2xl"></i>
             </div>
             <div id="acc3" :class="{ 'acc3Show': acc3, 'acc3Hidden': !acc3 }" class="bg-stone-200">
                 <p class="p-3">Sí, todos nuestros relojes vienen con una garantía de un año que cubre defectos de fabricación.</p>
